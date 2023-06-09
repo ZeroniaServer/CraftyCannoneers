4a08cd461d745288ea525b10cf306d74e60b9066
Evtema3 <evtema3@gmail.com>
2023-06-09 00:20:30 -0400

    fix remaining stability issues with lightning

--- craftycannoneers/data/game/functions/boat/rotate/action.py
+++ craftycannoneers/data/game/functions/boat/rotate/action.py
@@ -3,4 +3,4 @@ for i in range(-90,90):
     g.write("execute if entity @s[type=boat,tag=Replace] run function game:boat/rotate/placeboat/%d\n" % i)
     g.write("execute if entity @s[type=marker,tag=respawnboat] run function game:boat/rotate/placeboat/%d\n" % i)
     g.write("execute if entity @s[type=villager,tag=BoatCannon] run function game:boat/rotate/replacedisplay/%d\n" % i)
-    g.write("execute if entity @s[type=boat,tag=Shoot] run function game:boat/rotate/firecannonball/%d\n" % i)
+    g.write("execute if entity @s[type=boat,tag=Shoot] run function game:boat/rotate/firecannonball/%d" % i)
